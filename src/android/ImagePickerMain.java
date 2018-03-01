c62ee3f1b8bcb3eb66fd2a11d57c10d6342d2c56
baosheng.peng@haoqitech.com.cn <879236589>
2018-03-01 16:49:14 +0800

    插件完善

--- src/android/ImagePickerMain.java
+++ src/android/ImagePickerMain.java
@@ -6,6 +6,7 @@ import com.rxxb.imagepicker.bean.ImageItem;
 import com.rxxb.imagepicker.crop.AspectRatio;
 import com.rxxb.imagepicker.loader.GlideImageLoader;
 import com.rxxb.imagepicker.ui.ImageGridActivity;
+import com.rxxb.imagepicker.util.BitmapUtil;
 import com.rxxb.imagepicker.view.CropImageView;
 
 import org.apache.cordova.CallbackContext;
@@ -38,12 +39,35 @@ public class ImagePickerMain extends CordovaPlugin {
                 if (params.has("cutType")) {
                     cutType = params.optInt("cutType", 2);
                 }
-                if (params.has("width")) {
-                    imagePicker.setOutPutX(params.optInt("width"));
+                imagePicker.setOutPutX(params.optInt("width", 0));
+                imagePicker.setOutPutY(params.optInt("height", 0));
+
+                //颜色相关设置
+                if (params.has("oKButtonTitleColorNormal")) {
+                    imagePicker.getViewColor().setoKButtonTitleColorNormal(params.getString("oKButtonTitleColorNormal"));
                 }
-                if (params.has("height")) {
-                    imagePicker.setOutPutY(params.optInt("height"));
+                if (params.has("oKButtonTitleColorDisabled")) {
+                    imagePicker.getViewColor().setoKButtonTitleColorDisabled(params.getString("oKButtonTitleColorDisabled"));
                 }
+                if (params.has("naviBgColor")) {
+                    imagePicker.getViewColor().setNaviBgColor(params.getString("naviBgColor"));
+                }
+                if (params.has("naviTitleColor")) {
+                    imagePicker.getViewColor().setNaviTitleColor(params.getString("naviTitleColor"));
+                }
+                if (params.has("barItemTextColor")) {
+                    imagePicker.getViewColor().setBarItemTextColor(params.getString("barItemTextColor"));
+                }
+                if (params.has("toolbarBgColor")) {
+                    imagePicker.getViewColor().setToolbarBgColor(params.getString("toolbarBgColor"));
+                }
+                if (params.has("toolbarTitleColorNormal")) {
+                    imagePicker.getViewColor().setToolbarTitleColorNormal(params.getString("toolbarTitleColorNormal"));
+                }
+                if (params.has("toolbarTitleColorDisabled")) {
+                    imagePicker.getViewColor().setToolbarTitleColorDisabled(params.getString("toolbarTitleColorDisabled"));
+                }
+
                 if (cutType == 0) {
                     //圆形单选
                     imagePicker.setStyle(CropImageView.Style.CIRCLE);
@@ -78,20 +102,37 @@ public class ImagePickerMain extends CordovaPlugin {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-        if (resultCode == ImagePicker.RESULT_CODE_ITEMS) {
-            if (intent != null && requestCode == 100) {
-                ArrayList<ImageItem> images = (ArrayList<ImageItem>) intent.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
-                ArrayList<String> imagePath = new ArrayList();
-                if (images != null && !images.isEmpty()) {
-                    for (ImageItem image : images) {
-                        imagePath.add(image.path);
-                    }
-                }
-                JSONArray res = new JSONArray(imagePath);
-                mCallbackContext.success(res);
-            } else {
-                mCallbackContext.error("获取获取到图片数据");
+        if (resultCode == ImagePicker.RESULT_CODE_ITEMS && intent != null && requestCode == 100) {
+            final ArrayList<ImageItem> images = (ArrayList<ImageItem>) intent.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
+            if (images == null || images.isEmpty()) {
+                mCallbackContext.error("未获取到图片数据");
+                return;
             }
+            cordova.getThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    ArrayList<String> imagePath = new ArrayList();
+                    for (ImageItem image : images) {
+                        String result = image.path;
+                        String destPath = ImagePicker.createFile(cordova.getContext().getExternalCacheDir(), "IMG_"+System.currentTimeMillis(), ".png").getAbsolutePath();
+                        boolean saved;
+                        if (ImagePicker.getInstance().isOrigin() || ImagePicker.getInstance().getOutPutX() == 0 || ImagePicker.getInstance().getOutPutY() == 0) {
+                            //原图按图片原始尺寸压缩
+                            saved = BitmapUtil.saveBitmap2File(BitmapUtil.compress(result), destPath);
+                        } else {
+                            //按给定的宽高压缩
+                            saved = BitmapUtil.saveBitmap2File(BitmapUtil.getScaledBitmap(result, ImagePicker.getInstance().getOutPutX(), ImagePicker.getInstance().getOutPutY()), destPath);
+                        }
+                        if (outputType == 0) {
+                            imagePath.add(saved ? destPath : result);
+                        } else {
+                            imagePath.add(BitmapUtil.base64Image(saved ? destPath : result));
+                        }
+                    }
+                    JSONArray res = new JSONArray(imagePath);
+                    mCallbackContext.success(res);
+                }
+            });
         } else {
             mCallbackContext.error("获取失败");
         }
